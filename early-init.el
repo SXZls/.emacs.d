f3ff4a369bc5cc79e83b860ba91c3a162d6dcb50
srq <2490063603@qq.com>
2025-12-22 20:03:23 +0800

    more original

--- early-init.el
+++ early-init.el
@@ -1,40 +1,24 @@
-(setq gc-cons-threshold most-positive-fixnum)
-(require 'cl-lib)
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+
+(setq
+ native-comp-deferred-compilation nil
+ native-comp-jit-compilation nil ;; Prevent unwanted runtime compilation for gccemacs (native-comp) users;
+;; packages are compiled ahead-of-time when they are installed and site files
+;; are compiled when gccemacs is installed.
+ frame-inhibit-implied-resize t
+ 
+ initial-major-mode 'fundamental-mode
+ 
+ package-enable-at-startup nil
+ package--init-file-ensured t
+ use-package-enable-ime-menu-support t
+
+ auto-mode-case-fold nil
+ 
+ warning-suppress-types '((files))) ;; no lexical binding
 
 (prefer-coding-system 'utf-8)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
-
-(defun add-subdirs-to-load-path (search-dir)
-  (interactive)
-  (let* ((dir (file-name-as-directory search-dir)))
-    (dolist (subdir
-             ;; Filter out unnecessary directories to improve Emacs startup speed
-             (cl-remove-if
-              #'(lambda (subdir)
-                  (or
-                   ;; Remove files that are not directories
-                   (not (file-directory-p (concat dir subdir)))
-                   ;; Remove parent, language-related, and version control directories
-                   (member subdir '("." ".." 
-                                    "dist" "node_modules" "__pycache__" 
-                                    "RCS" "CVS" "rcs" "cvs" ".git" ".github" "el~" "EL~")))) 
-              (directory-files dir)))
-      (let ((subdir-path (concat dir (file-name-as-directory subdir))))
-        ;; Only add paths with .el .so .dll files into `load-path` to improve Emacs startup speed
-        (when (cl-some #'(lambda (subdir-file)
-                           (and (file-regular-p (concat subdir-path subdir-file))
-                                ;; .so .dll indicate dynamic libraries written in non-Elisp languages
-                                (member (file-name-extension subdir-file) '("el" "so" "dll"))))
-                       (directory-files subdir-path))
-
-          ;; Note: The third parameter of `add-to-list` must be `t`, meaning adding to the end of the list
-          ;; This ensures that Emacs searches for Elisp plugins in order from parent to child directories.
-	  ;; Reversing the order may cause Emacs to fail to start properly.
-          (add-to-list 'load-path subdir-path t))
-
-        ;; Continue recursively searching subdirectories
-        (add-subdirs-to-load-path subdir-path)))))
-
-(add-subdirs-to-load-path (expand-file-name "~/.emacs.d/"))
--- init.el
+++ init.el
@@ -1,15 +1,50 @@
-(require 'init-accelerate)
 
 (add-to-list 'default-frame-alist '(font . "Unifont-16"))
 
 
-(setq gc-cons-threshold most-positive-fixnum)
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
 
-(require 'no-littering)
+(require 'cl-lib)
+(defun add-subdirs-to-load-path (search-dir)
+  (interactive)
+  (let* ((dir (file-name-as-directory search-dir)))
+    (dolist (subdir
+             ;; Filter out unnecessary directories to improve Emacs startup speed
+             (cl-remove-if
+              #'(lambda (subdir)
+                  (or
+                   ;; Remove files that are not directories
+                   (not (file-directory-p (concat dir subdir)))
+                   ;; Remove parent, language-related, and version control directories
+                   (member subdir '("." ".." 
+                                    "dist" "node_modules" "__pycache__" 
+                                    "RCS" "CVS" "rcs" "cvs" ".git" ".github" "el~" "EL~")))) 
+              (directory-files dir)))
+      (let ((subdir-path (concat dir (file-name-as-directory subdir))))
+        ;; Only add paths with .el .so .dll files into `load-path` to improve Emacs startup speed
+        (when (cl-some #'(lambda (subdir-file)
+                           (and (file-regular-p (concat subdir-path subdir-file))
+                                ;; .so .dll indicate dynamic libraries written in non-Elisp languages
+                                (member (file-name-extension subdir-file) '("el" "so" "dll"))))
+                       (directory-files subdir-path))
+
+          ;; Note: The third parameter of `add-to-list` must be `t`, meaning adding to the end of the list
+          ;; This ensures that Emacs searches for Elisp plugins in order from parent to child directories.
+	  ;; Reversing the order may cause Emacs to fail to start properly.
+          (add-to-list 'load-path subdir-path t))
+
+        ;; Continue recursively searching subdirectories
+        (add-subdirs-to-load-path subdir-path)))))
+
+(add-subdirs-to-load-path (expand-file-name "~/.emacs.d/"))
+
 
 (require 'init-default)
 
 (require 'init-package)
 
+(use-package no-littering
+  :ensure t)
 
 (provide 'init)
