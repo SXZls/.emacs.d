71f3a3f7b4e95a0d0ee2bfdd55246136c16e1d75
srq <2490063603@qq.com>
2026-01-29 22:06:45 +0800

    add qcl

--- var/ido-save-directory-list.el
+++ var/ido-save-directory-list.el
@@ -2,18 +2,23 @@
 
 ;; ----- ido-last-directory-list -----
 (
- ("/home/srq/.emacs.d/" . "lisp/")
- ("/home/srq/" . ".emacs.d/")
+ ("/home/srq/.emacs.d/var/" . "eshell/")
+ ("/home/srq/.emacs.d/" . "var/")
+ ("/home/srq/" . ".ssh/")
 )
 
 ;; ----- ido-work-directory-list -----
 (
+ "/home/srq/.emacs.d/var/eshell/"
+ "/home/srq/.ssh/"
  "/home/srq/.emacs.d/"
  "/home/srq/.emacs.d/lisp/"
 )
 
 ;; ----- ido-work-file-list -----
 (
+ "history"
+ "known_hosts"
  "init.el"
  "init-function.el"
  "init-package.el"
@@ -21,13 +26,19 @@
 
 ;; ----- ido-dir-file-cache -----
 (
- ("/home/srq/.emacs.d/" (27003 11370 164095 653000) "./" "init.el" ".git/" "lisp/" "ido.last" "early-init.el" "etc/" "elpa/" "auto-save-list/" ".gitignore" "site-lisp/" "../" ".gitignore~" "var/")
+ ("/home/srq/.emacs.d/var/eshell/" (27002 4532 961253 322000) "./" "history" "../")
+
+ ("/home/srq/.emacs.d/var/" (27003 11424 26495 218000) "eshell/" "./" "gnus/" "eww/" "org/" "geiser/" "auto-save/" "transient/" "ido-save-directory-list.el" "../" "sly/" "ido-save-directory-list.el~")
+
+ ("/home/srq/.emacs.d/" (27003 11765 521974 364000) "./" "init.el" ".git/" "lisp/" "ido.last" "early-init.el" "etc/" "elpa/" "auto-save-list/" ".gitignore" "site-lisp/" "../" ".gitignore~" "var/")
+
+ ("/home/srq/.ssh/" (26953 15747 302779 990000) "./" "known_hosts.old" "known_hosts" "../" "known_hosts~")
 
  ("/home/srq/" (27003 11318 251793 442000) ".bashrc" "./" ".local/" "webfonts/" "vm/" ".oldemacs" "learn/" ".w3m/" ".fbtermrc" "csv10.3.0/" ".bashrc.bak" "wget-log" ".pinerc" ".config/" ".bash_logout" ".motd_shown" ".sudo_as_admin_successful" ".vscode-remote-containers/" ".dotnet/" ".gitconfig" "gforth/" ".npm/" "#.oldemacs#" ".emacs.d/" ".akku/" "Downloads/" "emacs-30.2/" ".vscode-server/" "LICENSE" "akku/" "sam.save" ".viminfo" ".landscape/" "acme.dump" "Akku.lock" "mail/" ".wget-hsts" "version-check.sh" ".emacs~" ".vim/" ".swp" "racket-9.0-x86_64-linux-buster-cs.sh" ".geiser_history.chez" ".bash_history" ".cache/" ".profile" "dead.letter" "../" ".ssh/" ".chezscheme_history" ".guix-profile/" ".lesshst" "Akku.manifest" "scheme-langserver/" ".gforth-history" "uemacs/" ".vimrc")
 
- ("/home/srq/.emacs.d/lisp/" (27002 53887 687254 285000) "init-function.el~" "./" "init-package.el~" "init-cl.el" "init-package.el" "custom.el" "init-function.el" "../" "custom.el~")
-
  ("/home/srq/learn/" (27003 11318 251793 442000) "./" "GUI.cpp" "2" "expline.c~" "x.lisp~" "Programming_Qt.sln" "main.cpp~" "main.c~" "Window.cpp" "x.scm~" "#main.cpp#" "#x.org#" "sum.c~" "~~sam~~" "x.lisp" "rcomment.c" "detab.c" "increment.c" "kp.c" "tree.c" "increment.c~" "sum.c" "main.c" "Colormap.cpp" "sonnet" "Simple_window.cpp" "expline.c" "utils/" "eopl3/" "#max.c#" "detab.c~" "y.c" "CMakeLists.txt" "entab.c~" "entab.c" "Programming_Qt.vcxproj" "life.c" "x.scm" "two.c~" "main.cpp" "#x.scm#" "#main.c#" "sum" "y.c~" "life.c~" "two.c" "#kp.c#" "../" "Graph.cpp" "PPP/" "a.out")
+
+ ("/home/srq/.emacs.d/lisp/" (26953 15837 20833 517000) "./" "init-package.el~" "init-package.el" "custom.el" "init-function.el" "../" "custom.el~")
 )
 
 ;; ----- ido-unc-hosts-cache -----
