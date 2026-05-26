0e51a10a650405e840095a0521ed23e23e2b9913
srq <2490063603@qq.com>
2026-05-26 20:16:58 +0800

    one config file

--- init.el
+++ init.el
@@ -1,11 +1,8 @@
 ;; -*- lexical-binding: t; -*-
-
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.6
       file-name-handler-alist nil
       jit-lock-defer-time 0
-      ;read-process-output-max (* 4 1024 1024)
-      ;process-adaptive-read-buffering nil  ;; LSP config
       )
 (add-hook 'emacs-startup-hook
 	  (lambda ()
@@ -31,16 +28,268 @@
 ;; Ensure these functions are called after `package-initialize`
 (advice-add #'package-initialize :after #'add-subdirs-to-load-path)
 
-;(require 'cl-lib)
+;;;;;;;;;;
+;; package
+(require 'package)
 
-(require 'init-package)
+(add-to-list 'package-archives '("melpa"  . "https://melpa.org/packages/") t)
 
-;(require 'init-utils)
+(package-initialize)
 
-(require 'init-enhance)
+(defvar *package-lists-fetched* nil)
 
-(require 'init-edit)
+(defun soft-fetch-package-lists ()
+  (unless *package-lists-fetched*
+    (package-refresh-contents)
+    (setf *package-lists-fetched* t)))
 
-(require 'init-lisp)
+;; package-installed-p will always report NIL if a newer
+;; version is available. We do not want that.
+(defun package-locally-installed-p (package)
+  (assq package package-alist))
+
+(defun ensure-installed (&rest packages)
+  (unless (cl-loop for package in packages
+                   always (package-locally-installed-p package))
+    (soft-fetch-package-lists)
+    (dolist (package packages)
+      (unless (package-locally-installed-p package)
+        (package-install package)))))
+;;;;;;;;;;
+;; enhance
+(setq-default fill-column 80
+              indent-tabs-mode nil
+              abbrev-mode t)
+
+(recentf-mode 1) ;; recentf file
+(save-place-mode 1)
+(electric-pair-mode 1)
+;(global-display-line-numbers-mode t)
+(column-number-mode 1)
+(ido-mode 1)
+(global-font-lock-mode 1)
+(transient-mark-mode 1)
+(auto-compression-mode 1)
+(auto-fill-mode 1)
+(prefer-coding-system 'utf-8)
+(auto-image-file-mode 1)
+
+(setq uniquify-buffer-name-style 'forward
+      tab-always-indent 'complete
+      visible-bell 1
+      track-eol 1
+      kill-whole-line 1
+      sentence-end
+      "\\([。！？]\\|……\\|[.?!][]\"')}]*\\($\\|[ \t]\\)\\)[ \t\n]*"
+      sentence-end-double-space nil
+      ;; Setting sentence-end can recognize Chinese punctuation.
+      ;There is no need to insert two spaces when filling.
+      enable-recursive-minibuffers t
+      scroll-conservatively 97
+      scroll-preserve-screen-position t
+      treesit-extra-load-path (executable-find "tree-sitter")
+      save-abbrevs nil ;; abbrev
+      my-abbrevs-file (expand-file-name
+                       "site-lisp/abbrevs.el"
+                       user-emacs-directory);; abbrev
+      isearch-allow-scroll t
+      )
+
+(if (file-exists-p my-abbrevs-file)
+    (read-abbrev-file my-abbrevs-file))
+
+(global-set-key (kbd "M-/") 'dabbrev-expand)
+
+(put 'set-goal-column 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+;;; dired
+(add-hook 'dired-load-hook
+          (function (lambda ()
+                      (load "dired-x"))))
+(setq dired-recursive-copies 'top
+      dired-recursive-deletes 'top
+      dired-use-ls-dired nil)
+
+(set-face-attribute 'default nil
+                    :background (if (display-graphic-p) "#111" "unspecified")
+                    :foreground (if (display-graphic-p) "wheat" "#fff")
+		    :family "juliamono"
+		    :height 130)
+
+(defun to-unifont()
+  (interactive)
+  (buffer-face-set '(:family "Unifont" :height 160)))
+(global-set-key (kbd"C-c u")
+                'to-unifont)
+
+(setq custom-file (expand-file-name "custom.el"
+                                    (concat user-emacs-directory "site-lisp/")))
+(when (file-exists-p custom-file)
+ (load-file custom-file))
+
+(mapcar
+ (function (lambda (setting)
+             (setq auto-mode-alist
+                   (cons setting auto-mode-alist))))
+ '(("\\.l\\'" . c-mode)
+   ("\\.cl\\'" . lisp-mode)
+   ("\\.lisp\\'" . lisp-mode)
+   ("\\.scm\\'" . scheme-mode)
+   ("\\.rkt\\'" . scheme-mode)
+   ("\\.ss\\'" . scheme-mode)))
+;; easy to add-mode-list.
+
+;;;;;;;;
+;; magit
+(ensure-installed 'magit)
+;(require 'magit)
+(autoload 'magit-status "magit" "Magit status." t)
+(autoload 'magit-blame "magit" "Blame current file." t)
+(autoload 'magit-log-buffer-file "magit" "Log current file." t)
+(autoload 'magit-dispatch "magit" "Magit dispatch." t)
+(autoload 'magit-file-dispatch "magit" "Magit file dispatch." t)
+(global-set-key (kbd "C-x g") 'magit-status)
+(with-eval-after-load 'magit
+  (setq magit-auto-select-connection 'always)
+  ;; other configs
+  )
+
+;;;;;;;;;;;;
+;; yasnippet
+(ensure-installed 'yasnippet)
+;(require 'yasnippet)
+;(add-to-list 'load-path "~/.emacs.d/site-lisp/")
+(autoload 'yas-minor-mode "yasnippet" "YASnippet minor mode." t)
+(autoload 'yas-reload-all "yasnippet" nil t)
+
+(add-hook 'prog-mode-hook 'yas-minor-mode)
+(add-hook 'Latex-mode-hook 'yas-minor-mode)
+
+;;;;;;;;;;
+;; paredit
+(ensure-installed 'paredit)
+;(require 'paredit)
+;(add-to-list 'load-path "~/.emacs.d/site-lisp/")
+
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+
+(add-hook 'lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+;(add-hook 'slime-repl-mode-hook 'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook 'enable-paredit-mode)
+(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'ielm-mode-hook 'enable-paredit-mode)
+(add-hook 'scheme-mode-hook 'enable-paredit-mode)
+
+(put 'paredit-forward-delete 'delete-selection 'supersede)
+(put 'paredit-backward-delete 'delete-selection 'supersede)
+(put 'paredit-newline 'delete-selection t)
+
+;;;;;;;;;
+;; Scheme 
+;(require 'cmuscheme)
+(autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process" t)
+(autoload 'scheme-mode "cmuscheme" "Scheme mode" t)
+
+(setq scheme-program-name
+      (cond
+       ((executable-find "scheme"))
+       ((executable-find "racket"))
+       ((executable-find "guile"))
+       (t "scheme")))
+
+(defun use-racket()
+  (interactive)
+  (setq scheme-program-name (executable-find "racket"))
+  (message "to racket"))
+
+(defun use-chez()
+  (interactive)
+  (setq scheme-program-name (executable-find "scheme"))
+  (message "to chez"))
+
+(defun use-guile()
+  (interactive)
+  (setq scheme-program-name (executable-find "guile"))
+  (message "to guile"))
+
+;; bypass the interactive question and start the default interpreter
+(defun scheme-proc ()
+  "Return the current Scheme process, starting one if necessary."
+  (unless (and scheme-buffer
+               (get-buffer scheme-buffer)
+               (comint-check-proc scheme-buffer))
+    (save-window-excursion
+      (run-scheme scheme-program-name)))
+  (or (scheme-get-process)
+      (error "No current process. See variable `scheme-buffer'")))
+
+(defun switch-other-window-to-buffer (name)
+    (other-window 1)
+    (switch-to-buffer name)
+    (other-window 1))
+
+(defun scheme-split-window ()
+  (cond
+   ((= 1 (count-windows))
+    (split-window-vertically (floor (* 0.68 (window-height))))
+    ;; (split-window-horizontally (floor (* 0.5 (window-width))))
+    (switch-other-window-to-buffer "*scheme*"))
+   ((not (member "*scheme*"
+               (mapcar (lambda (w) (buffer-name (window-buffer w)))
+                       (window-list))))
+    (switch-other-window-to-buffer "*scheme*"))))
+
+(defun scheme-send-last-sexp-split-window ()
+  (interactive)
+  (scheme-split-window)
+  (scheme-send-last-sexp))
+
+(defun scheme-send-definition-split-window ()
+  (interactive)
+  (scheme-split-window)
+  (scheme-send-definition))
+
+(add-hook 'scheme-mode-hook
+  (lambda ()
+    ;(paredit-mode 1)
+    (define-key scheme-mode-map (kbd "C-c C-s")
+                'scheme-send-last-sexp-split-window)
+    (define-key scheme-mode-map (kbd "C-c C-d")
+                'scheme-send-definition-split-window)
+    (local-set-key (kbd "C-c c") 'use-chez)
+    (local-set-key (kbd "C-c r") 'use-racket)
+    (local-set-key (kbd "C-c g") 'use-guile)))
+
+;;;;;;;;;;;;;;
+;; common lisp
+(remove-hook 'lisp-mode-hook 'cl-lisp-mode-hook)
+
+(ensure-installed 'sly)
+;(require 'sly)
+(autoload 'sly "sly" "Start SLY" t)
+(autoload 'sly-mode "sly" "SLY mode" t)
+
+(setq sly-auto-select-connection 'always)
+;(setq sly-kill-without-query-p t)
+(setq sly-description-autofocus t) 
+(setq sly-inhibit-pipelining nil)
+(setq sly-load-failed-fasl 'always)
+
+;; XXX: Work around a bug in SLY whereby installing/compiling it as
+;; above fails to correctly set version (not very problematic for
+;; Portacle, which always ensures matching versions anyway)
+(setq sly-ignore-protocol-mismatches t)
+
+;; Make sure SLY knows about our SBCL
+(setq sly-lisp-implementations
+      `((sbcl (,(executable-find "sbcl") "--dynamic-space-size" "256"))))
+
+;; Don't turn on paredit in REPL, but at least use electric-pair-mode
+(add-hook 'sly-mrepl-mode-hook 'electric-pair-local-mode)
+
+;; Make sure we don't clash with SLIME when starting
+(add-hook 'lisp-mode-hook 'sly-mode)
 
 (provide 'init)
